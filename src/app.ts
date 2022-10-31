ef1fbfc4ce62305b2988e6c99a58f1c66c6e9567
Carlos Raúl Cárcamo Tejada <carlosraulcarcamotejada@gmail.com>
2022-10-31 15:19:01 -0600

    Primer commit

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import Express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const app = () => {
+  const express = Express();
+
+  express.use(cors());
+
+  express.use(Express.json());
+
+  express.listen(process.env.PORT, () => {
+    console.log(`Server on port: ${process.env.PORT}`);
+  });
+};
